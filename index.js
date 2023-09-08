f75feef1e4a061b27c2afb3e8970b6ad5da4f852
najmulazka <najmulazka225@gmail.com>
2023-09-09 00:33:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Najmul Azka inputan prompt bangun datar
+window.prompt = `Pilih proses hitung
+1. Luas persegi
+2. Luas persegi panjang
+3. Luas segi tiga
+4. luas lingkaran
+5. keliling persegi
+6. keliling persegi panjang
+7. keliling segi tiga
+8. keliling lingkaran`;
