fef7ada9a77875beaaab2880c68093b262b72cfd
najmulazka <najmulazka225@gmail.com>
2023-09-09 23:56:38 +0700

    Menghapus class bangun di program.js dan membersihkan code di index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { Bangun, Persegi, Segitiga, PersegiPanjang, Lingkaran } from './program.js';
+import { Persegi, Segitiga, PersegiPanjang, Lingkaran } from './program.js';
 
 // Najmul Azka inputan prompt bangun datar
 async function main() {
     try {
-        let bangun1 = new Bangun();
         const persegi = new Persegi();
         const segitiga = new Segitiga();
         const persegiPanjang = new PersegiPanjang();
@@ -25,50 +24,56 @@ async function main() {
             let result;
             switch (menu) {
                 case 1:
-                    let sisi = window.prompt('Masukan panjang sisi');
-                    result = await bangun1.luasPersegi(sisi);
+                    persegi.sisi = window.prompt('Masukan panjang sisi');
+                    result = await persegi.luas();
                     alert(result);
                     break;
 
-                // najmul azka -> add case 2
                 case 2:
-                    let panjang = window.prompt('Masukan panjang sisi');
-                    let lebar = window.prompt('Masukan lebar sisi');
-                    result = await bangun1.luasPersegiPanjang(panjang, lebar);
+                    persegiPanjang.panjang = window.prompt('Masukan panjang sisi');
+                    persegiPanjang.lebar = window.prompt('Masukan lebar sisi');
+                    result = await persegiPanjang.luas();
                     alert(result);
                     break;
+
                 case 3:
                     segitiga.alas = window.prompt('Masukan alas segitiga');
                     segitiga.tinggi = window.prompt('Masukan tinggi segitiga');
                     result = await segitiga.luas();
                     alert(result);
                     break;
+
                 case 4:
                     lingkaran.jariJari = window.prompt('Masukan jari jari lingkaran');
                     result = await lingkaran.luas();
                     alert(result);
                     break;
+
                 case 5:
                     persegi.sisi = window.prompt('Masukan sisi persegi');
                     result = await persegi.keliling();
                     alert(result);
                     break;
+
                 case 6:
                     persegiPanjang.panjang = window.prompt('Masukan panjang persegi panjang');
                     persegiPanjang.lebar = window.prompt('Masukan lebar persegi panjang');
                     result = await persegiPanjang.keliling();
                     alert(result);
                     break;
+
                 case 7:
                     segitiga.sisi = window.prompt('Masukan sisi segitiga');
                     result = await segitiga.keliling();
                     alert(result);
                     break;
+
                 case 8:
                     lingkaran.jariJari = window.prompt('Masukan jari jari lingkaran');
                     result = await lingkaran.keliling();
                     alert(result);
                     break;
+
                 default:
                     alert('Menu tidak tersedia');
                     break;
--- program.js
+++ program.js
@@ -1,36 +1,3 @@
-export class Bangun {
-    // Najmul Azka -> feature luas persegi
-    luasPersegi(sisi) {
-        return new Promise((resolve, reject) => {
-            if (typeof sisi !== 'number') sisi = Number(sisi);
-            if (isNaN(sisi)) return reject('value sisi salah');
-
-            let luasPersegi = sisi * sisi;
-            console.log(`sisi = ${sisi}`);
-            return resolve(`luas persegi dengan sisi ${sisi} = ${luasPersegi}`);
-        });
-    }
-
-    luasPersegiPanjang(panjang, lebar) {
-        return new Promise((resolve, reject) => {
-            if (typeof panjang !== 'number') panjang = Number(panjang);
-            if (isNaN(panjang)) return reject('value panjang salah');
-            if (typeof lebar !== 'number') lebar = Number(lebar);
-            if (isNaN(lebar)) return reject('value lebar salah');
-
-            let luasPersegiPanjang = panjang * lebar;
-            console.log(`panjang = ${panjang}`);
-            console.log(`lebar = ${lebar}`);
-            return resolve(`luas persegi panjang dengan panjang ${panjang} dan lebar ${lebar} = ${luasPersegiPanjang}`);
-        });
-    }
-}
-
-/*
-    Karena bangun datar pasti mempunyai luas dan keliling,
-    maka kita buat abstract class BangunDatar yang berisi
-    method luas dan keliling yang harus diimplementasikan
-*/
 // Abstract Class
 class BangunDatar {
     constructor(name) {
